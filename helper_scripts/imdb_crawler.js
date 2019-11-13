5aa4f41df93ca7aa0609ae47b94bc934bfd410f5
Abhay Zala <zala.abhay@gmail.com>
2019-11-13 01:33:11 -0500

    Fixed bug

--- helper_scripts/imdb_crawler.js
+++ helper_scripts/imdb_crawler.js
@@ -5,8 +5,6 @@ function applyImdbThumbnail(div, isHome, id, oncomplete) {
 		success: function(response) {
 			var html = $.parseHTML(response);
 
-			console.log(response);
-
 			var img = response.replace(response.substring(0, response.indexOf("src=\"")+5), "");
 			img = img.substring(0, img.indexOf("\" />"));
 
@@ -14,7 +12,7 @@ function applyImdbThumbnail(div, isHome, id, oncomplete) {
 				img = img.replace("182,268", "675,1000");
 				img = img.replace(img.substring(img.indexOf("_U")+1, img.indexOf("CR")), "SY1000_");
 			}
-
+			
 			$(div).css("background", "url(" + img + ")");
 
 			oncomplete();
--- home_javascript.js
+++ home_javascript.js
@@ -29,19 +29,22 @@ function loadData() {
 	for (var i=0;i<slideshowElements.length;i++) {
 		var object = get_featured_movies(i);
 
+		// Apply text updates
 		slideshowElements[i].children[1].children[0].innerHTML = object.name;
 		slideshowElements[i].children[1].children[3].innerHTML = object.rating;
 		slideshowElements[i].children[1].children[6].innerHTML = object.desc;
 
+		// Call imdb_crawler to pull poster images
 		applyImdbThumbnail(slideshowElements[i].children[0].children[1], true, object.id, removeShine);
 	}
 }
 
 // Removes the shimmering effect and replaces it with the image
 function removeShine() {
-	$(slideshowElements[i].children[0].children[1]).removeClass("shine");
-	$(slideshowElements[i].children[0].children[1]).addClass("loaded");
-
+	for (var i=0;i<slideshowElements.length;i++) {
+		$(slideshowElements[i].children[0].children[1]).removeClass("shine");
+		$(slideshowElements[i].children[0].children[1]).addClass("loaded");
+	}
 }
 
 // Run on script load
