d85942f5a3f0c0e969f9a5d47c7784072b02ca25
Abhay Zala <zala.abhay@gmail.com>
2019-11-13 02:43:22 -0500

    Made recommend page. Optimizations. Updates. Comments

--- helper_scripts/pull_site.php
+++ helper_scripts/pull_site.php
@@ -1,13 +1,19 @@
 <?php
+	# get the url to pull from
 	$url = (isset($_GET['url'])) ? $_GET['url'] : false;
 
+	# if there is no url then exit script
 	if(!$url)
 		exit;
 
+	# pull file contents from URL
 	header('Content-Type: text/html');
 	$string = file_get_contents($url);
+
+	# extract the poster div
 	$first_step = explode( '<div class="poster">' , $string);
 	$second_step = explode("</div>" , $first_step[1] );
 
+	# print the contents in the poster div
 	echo $second_step[0];
 ?>
