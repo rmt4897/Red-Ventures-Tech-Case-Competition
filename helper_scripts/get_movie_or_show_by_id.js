7c385bf281f295e9691dd46d161c627459daa052
rmt4897 <46603141+rmt4897@users.noreply.github.com>
2019-11-13 16:50:55 -0500

    file to get movies or shows with type and id

---
+++ helper_scripts/get_movie_or_show_by_id.js
@@ -0,0 +1,4 @@
+// blank file that will retrieve a movie or show
+// when a function is called with parameters
+// (var type, var id), where 'type' is either 
+// "movie" or "show" and id is an imdb id
